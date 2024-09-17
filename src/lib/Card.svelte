a076eaa23db698fb37b197ac6efa8003ada448b7
Naddybs <144009698+Naddybs@users.noreply.github.com>
2024-09-17 17:47:47 +0200

    Data ophalen & weergeven

---
+++ src/lib/Card.svelte
@@ -0,0 +1,24 @@
+<!-- Hier exporteer ik de variabelen die ik in de Card.svelte component gebruik. -->
+<script>
+    export let name;
+    export let avatar;
+    export let prefix;
+    export let surname;
+    export let id;
+  </script>
+<!-- Hier maak ik de Card.svelte component.
+    - Ik implementeer de geexporteerde variabelen -->
+  <main class="card">
+    <section class="card-inner">
+      <article class="card-front">
+        <img 
+        src={avatar} 
+        alt="Profile picture of {name}" 
+        onerror="this.onerror=null;this.src='/pikachu.webp';" />
+        <a href='/{id}'>
+        <h2>{name}</h2>
+        </a>
+        <h3>{prefix}{surname}</h3>
+      </article>
+    </section>
+</main>
---
+++ src/routes/squad/[squad_id]/+page.svelte
@@ -0,0 +1,23 @@
+<!-- Hier importeer ik de Card.svelte component. -->
+<script>
+    export let data
+    import Card from "$lib/Card.svelte"
+  </script>
+  
+  <!-- Hier gebruik ik een each loop om de data.persons array te doorlopen
+    - voor elke persoon in de array maak ik een Card component aan
+    - ik geef de Card component props mee die ik herbruik in de Card.svelte component -->
+  <main>
+    <h1> Squad</h1>
+    <section class ="cards-container">
+      {#each data.persons as person, index}
+          <Card
+              name = {person.name}
+              avatar = {person.avatar}
+              prefix = {person.prefix}
+              surname = {person.surname}
+              id = {person.id}
+          />
+      {/each}
+    </section>
+  </main>
