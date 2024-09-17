7d40a5559a88bb41ebfc5176d02a667be721108a
RubenErhardt <rubengamebattles@outlook.com>
2024-09-17 20:11:20 +0200

    laatste versie voor nu
    - verschillende background
    - members count
    - tabfunctie
    - kaartjes draaien met tab
    - achtergrond radient
    - dynamisch squad nummer

--- src/routes/squad/[squad_id]/+page.svelte
+++ src/routes/squad/[squad_id]/+page.svelte
@@ -1,121 +1,175 @@
 <script>
-    export let data
+    export let data;
     import { onMount } from 'svelte';
 
-let backgroundColor = '';
+    onMount(() => {
+        const path = window.location.pathname;
+        let background = '';  // Achtergrond gradient
+        let squadName = '';
+        let memberCount = 0; // Houd het aantal leden bij
 
-// Update the background color based on the current path
-onMount(() => {
-    const path = window.location.pathname;
-    if (path.includes('squad/4')) {
-        backgroundColor = '#EC8146';
-    } else if (path.includes('squad/5')) {
-        backgroundColor = '#008000';
-    } else if (path.includes('squad/3')) {
-        backgroundColor = '#6890F0';
-    } else {
-        backgroundColor = '#FFFFFF'; 
-    }
-    document.body.style.backgroundColor = backgroundColor;
-});
+        if (path.includes('squad/4')) {
+            background = 'linear-gradient(to right, #EC8146, #FF5722)'; // Gradient van kleur
+            squadName = 'C';
+        } else if (path.includes('squad/5')) {
+            background = 'linear-gradient(to right, #008000, #4CAF50)'; // Gradient van kleur
+            squadName = 'D';
+        } else if (path.includes('squad/3')) {
+            background = 'linear-gradient(to right, #6890F0, #0033FF)'; // Gradient van kleur
+            squadName = 'Overig';
+        } else {
+            squadName = 'Niet beschikbaar'; // als er geen squad in het pad is
+        }
+
+        // Update de achtergrondkleur
+        document.body.style.background = background; // Instellen van achtergrondkleur als gradient
+        document.querySelector('header h1').textContent = `Squad ${squadName}`;
+        
+        // Update het aantal leden als de data beschikbaar is
+        if (data.persons) {
+            memberCount = data.persons.length;
+        }
+
+        // Update het aantal leden in de header
+        document.querySelector('#member-count').textContent = `Aantal pokemons: ${memberCount}`;
+    });
 </script>
 
-  <section class="kaartjes-container">
+<header>
+    <h1>Squad </h1>
+    <div id="member-count"></div>
+</header>
+
+<section class="kaartjes-container">
     {#if data.persons}
       {#each data.persons as person}
       <article class="kaartje-container">
-        <div class="kaartje">
-          <div class="kaartje-front">
-            <!-- Mogelijke content voor de voorkant -->
-          </div>
+        <button class="kaartje">
+          <div class="kaartje-front"></div>
           <div class="kaartje-back">
             <h2>{person.name} {person.prefix} {person.surname}</h2>
-            <a href="/{person.id}" class="button-unlock">Unlock Me</a>
+            <span class="button-unlock">Unlock Me</span>
           </div>
-        </div>
+        </button>
       </article>
       {/each}
     {:else}
       <p>No data available</p>
     {/if}
-  </section>
-  
-  
+</section>
+
 <style>
- .kaartjes-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px; /* Verminderde ruimte tussen kaartjes */
-  justify-content: center;
-  padding: 10px; /* Verminderde padding rond de container */
-}
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 
-.kaartje-container {
-  perspective: 800px; /* Kleinere perspectiefwaarde */
-  width: 200px; /* Kleinere breedte van kaartjes */
-  height: 250px; /* Kleinere hoogte van kaartjes */
-  box-sizing: border-box;
-}
+  header h1 {
+    margin-bottom: 10px; /* Verminder de ruimte onder de titel */
+    text-align: center; /* Zorg ervoor dat de titel gecentreerd is */
+    font-size: 3rem; /* Grotere tekstgrootte voor een opvallende titel */
+    font-weight: bold; /* Maak de tekst vetter */
+    color: #FFFFFF; /* Tekstkleur */
+    text-shadow: 3px 3px 6px rgba(0, 0, 0, 0.4); /* Voeg een schaduw toe voor diepte */
+    font-family: 'Arial', sans-serif; /* Gebruik een modern lettertype */
+    letter-spacing: 1px; /* Voeg wat ruimte tussen de letters toe */
+    padding: 10px; /* Voeg wat padding toe voor een betere uitstraling */
+    background: rgba(0, 0, 0, 0.3); /* Achtergrondkleur met doorzichtigheid voor extra contrast */
+    border-radius: 10px; /* Ronde hoeken voor een zachtere uitstraling */
+  }
 
-.button-unlock {
-  display: inline-block;
-  padding: 8px 16px; /* Kleinere padding voor de knop */
-  background-color: #ff0000; 
-  color: white; 
-  text-align: center;
-  text-decoration: none; 
-  outline: 2px solid black;
-  font-size: 14px; /* Kleinere lettergrootte voor de knop */
-  cursor: pointer;
-}
+  #member-count {
+    font-size: 1.5rem; /* Grotere tekstgrootte voor het aantal leden */
+    color: #FFFFFF; /* Tekstkleur */
+    background: rgba(0, 0, 0, 0.5); /* Achtergrondkleur met doorzichtigheid voor extra contrast */
+    padding: 5px 10px; /* Padding voor een betere uitstraling */
+    border-radius: 5px; /* Ronde hoeken */
+    margin-top: 10px; /* Ruimte boven het aantal leden */
+  }
 
-.kaartje {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  transform-style: preserve-3d;
-  transition: transform 0.6s;
-  cursor: pointer;
-}
+  .kaartjes-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center; /* Kaartjes netjes centreren */
+    padding: 0; /* Geen onnodige padding */
+    margin: 0 auto; /* Centreer de container zelf */
+  }
 
-.kaartje:hover {
-  transform: rotateY(180deg);
-}
+  .kaartje-container {
+    perspective: 800px;
+    width: 200px; /* Zorg ervoor dat de breedte van de kaartjes correct is */
+    height: 250px; /* Zorg ervoor dat de hoogte van de kaartjes correct is */
+    margin: 10px; /* Een beetje ruimte tussen kaartjes */
+    box-sizing: border-box;
+    position: relative; /* Zorg ervoor dat het binnen de container blijft */
+  }
 
-.kaartje-front,
-.kaartje-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-  border-radius: 6px; /* Kleinere radius voor de hoeken */
-}
+  .kaartje {
+    width: 100%;
+    height: 100%;
+    position: absolute; /* Zorg ervoor dat de kaartjes absoluut worden gepositioneerd binnen de container */
+    transform-style: preserve-3d;
+    transition: transform 0.6s;
+    background: transparent;
+    border: none;
+    padding: 0;
+    display: flex; /* Flexbox voor inhoud uitlijning */
+    align-items: center;
+    justify-content: center;
+  }
 
-.kaartje-front {
-  background-image: url('/61vOBvbsYJL._AC_UF1000,1000_QL80_.jpg');
-  background-size: cover;
-  background-position: center;
-  height: 250px; /* Kleinere hoogte voor de voorkant */
-  z-index: 1;
-}
+  .kaartje:focus,
+  .kaartje:hover {
+    transform: rotateY(180deg);
+  }
 
-.kaartje-back {
-  background: rgb(89, 51, 225);
-  display: flex;
-  height: 250px; /* Kleinere hoogte voor de achterkant */
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: rgb(255, 255, 255);
-  transform: rotateY(180deg);
-  text-align: center;
-  z-index: 2;
-}
+  .kaartje:focus {
+    outline: none;
+    box-shadow: 0 0 0 4px #ffffff; /* Voeg een nette focus-ring toe */
+  }
 
-.kaartje-back h2 {
-  font-size: 1em; /* Kleinere lettergrootte voor de tekst op de achterkant */
-  margin-bottom: 10px; /* Kleinere marge onder de tekst */
-}
+  .kaartje-front,
+  .kaartje-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    border-radius: 6px;
+  }
 
-    
-    </style>
+  .kaartje-front {
+    background-image: url('/61vOBvbsYJL._AC_UF1000,1000_QL80_.jpg');
+    background-size: cover;
+    background-position: center;
+    z-index: 1;
+  }
+
+  .kaartje-back {
+    background: rgba(114, 105, 146, 0.341);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    transform: rotateY(180deg);
+    text-align: center;
+    z-index: 2;
+  }
+
+  .kaartje-back h2 {
+    font-size: 1em;
+    margin-bottom: 10px;
+  }
+
+  .button-unlock {
+    display: inline-block;
+    padding: 8px 16px;
+    background-color: #ff0000;
+    color: white;
+    text-decoration: none;
+    outline: 2px solid black;
+    font-size: 14px;
+    cursor: pointer;
+  }
+</style>
